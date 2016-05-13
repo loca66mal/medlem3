bee12628330169d82a81e46b52a29922a17de93d
Loca <loca66mal@mail.com>
2016-05-13 14:36:05 +0200

    första två fönster

---
+++ medlemsprojektet/src/medlemsprojektet/Medlemsprojektet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medlemsprojektet;
+
+/**
+ *
+ * @author jpn
+ */
+public class Medlemsprojektet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//        InLoggning enInloggning = new InLoggning();
+//        enInloggning.setVisible(true);
+        new InLoggning().setVisible(true);
+
+    } 
+}
