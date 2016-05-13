7ab74ca7c6f8e2262e7d6e5d902a0c9e70b258cb
Loca <loca66mal@mail.com>
2016-05-13 15:42:43 +0200

    andra två fönster nya

--- medlemsprojektet/src/medlemsprojektet/NewJFrameMaster.java
+++ medlemsprojektet/src/medlemsprojektet/NewJFrameMaster.java
@@ -9,12 +9,12 @@ package medlemsprojektet;
  *
  * @author jpn
  */
-public class NewJFrameAnnan extends javax.swing.JFrame {
+public class NewJFrameMaster extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrameAnnan() {
+    public NewJFrameMaster() {
         initComponents();
     }
 
@@ -60,21 +60,23 @@ public class NewJFrameAnnan extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrameAnnan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrameMaster.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrameAnnan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrameMaster.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrameAnnan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrameMaster.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrameAnnan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrameMaster.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrameAnnan().setVisible(true);
+                new NewJFrameMaster().setVisible(true);
             }
         });
     }
